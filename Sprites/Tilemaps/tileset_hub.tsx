23c2c239ebe92a99c90bd859c23a426c699de087
BrianCompau <brcompau@gmail.com>
2025-03-03 21:38:55 -0500

    imported sprites

---
+++ Sprites/Tilemaps/tileset_hub.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Top_Down_Tiles_Part_3_Tileset lighthouse" tilewidth="16" tileheight="16" tilecount="110" columns="5">
+ <image source="lighthouse.png" width="80" height="352"/>
+</tileset>
